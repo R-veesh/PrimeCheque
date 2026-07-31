716f1b51b2cf21c199c97ac2faf272af1da1be6c
R-veesh <raweeshalakruvan15@gmail.com>
2026-07-31 12:57:10 +0530

    feat: implement cheque preview and print preview UI pages with WebView2 PDF rendering

--- PrimeCheque/Views/PrintPreviewPage.xaml.cs
+++ PrimeCheque/Views/PrintPreviewPage.xaml.cs
@@ -14,6 +14,26 @@ namespace PrimeCheque.Views
             InitializeComponent();
             ViewModel = App.GetService<PrintPreviewViewModel>();
             DataContext = ViewModel;
+            ViewModel.PropertyChanged += ViewModel_PropertyChanged;
+        }
+
+        private async void ViewModel_PropertyChanged(object? sender, System.ComponentModel.PropertyChangedEventArgs e)
+        {
+            if (e.PropertyName == nameof(ViewModel.PdfFilePath) && !string.IsNullOrEmpty(ViewModel.PdfFilePath))
+            {
+                try
+                {
+                    await PdfWebView.EnsureCoreWebView2Async();
+                    if (System.IO.File.Exists(ViewModel.PdfFilePath))
+                    {
+                        PdfWebView.CoreWebView2.Navigate(new Uri(ViewModel.PdfFilePath).AbsoluteUri);
+                    }
+                }
+                catch
+                {
+                    // Ignore navigation errors
+                }
+            }
         }
 
         protected override async void OnNavigatedTo(NavigationEventArgs e)
