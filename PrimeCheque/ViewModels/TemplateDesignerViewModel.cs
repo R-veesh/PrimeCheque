525b787ad8ede46b3afd4cbff7e4fd38c25a9dcc
R-veesh <raweeshalakruvan15@gmail.com>
2026-09-02 01:09:25 +0530

    refactor: remove unused property change handlers for cheque dimensions in TemplateDesignerViewModel

--- PrimeCheque/ViewModels/TemplateDesignerViewModel.cs
+++ PrimeCheque/ViewModels/TemplateDesignerViewModel.cs
@@ -36,22 +36,8 @@ namespace PrimeCheque.ViewModels
         [ObservableProperty]
         private double _chequeWidthMm = 200;
 
-        partial void OnChequeWidthMmChanged(double value)
-        {
-            OnPropertyChanged(nameof(ScaleFactor));
-            OnPropertyChanged(nameof(CanvasHeight));
-            UpdateFieldScales();
-        }
-
         [ObservableProperty]
         private double _chequeHeightMm = 88;
-
-        partial void OnChequeHeightMmChanged(double value)
-        {
-            OnPropertyChanged(nameof(ScaleFactor));
-            OnPropertyChanged(nameof(CanvasHeight));
-            UpdateFieldScales();
-        }
 
         [ObservableProperty]
         private string? _templateImagePath;
