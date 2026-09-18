c61f63f70d91f1bafe43fd8915f6b8be815a57e0
R-veesh <raweeshalakruvan15@gmail.com>
2026-09-18 17:17:26 +0530

    feat: Add conditional Edit button for Draft cheques in ChequeRegister

---
+++ PrimeCheque/Converters/ChequeStatusToVisibilityConverter.cs
@@ -0,0 +1,28 @@
+using System;
+using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Data;
+using PrimeCheque.Models;
+
+namespace PrimeCheque.Converters
+{
+    public class ChequeStatusToVisibilityConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            if (value is ChequeStatus status && parameter is string targetStatusStr)
+            {
+                if (Enum.TryParse<ChequeStatus>(targetStatusStr, out var targetStatus))
+                {
+                    return status == targetStatus ? Visibility.Visible : Visibility.Collapsed;
+                }
+            }
+            return Visibility.Collapsed;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
+
